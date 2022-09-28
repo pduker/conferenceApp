37630763f6c6d233dde725cd9459018cfee35a21
Jaydon Reap <jaydonreap@gmail.com>
2022-09-28 15:31:50 -0400

    Added promise support to parseDocx for ease of use

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,36 @@
-const { exec } = require("child_process")
+const { exec } = require('child_process')
 
+async function parseDocx (localFileName) {
 
-async function parseDocx (localFilePath) {
-  console.log("Sup")
+  if (!localFileName) {
+    throw new Error('A valid file name was not passed!')
+  }
 
+  // Wrap this exec callback function in a promise so it's easier to work with
+  const output = await new Promise(function (resolve, reject) {
+    exec(`pandoc ${localFileName} -f `, function (internalError, stdout, stderr){
+      if (internalError) {
+        console.error(internalError)
+        reject(internalError)
+      }
+  
+      if (stderr) {
+        console.error(stderr)
+        reject(stderr)
+      }
+      
+      if (stdout) {
+        resolve(stdout)
+      } else {
+        console.error('No response was sent from pandoc!')
+        reject()
+      }
+    })
+  })
 
+  if (!output) {
+    throw new Error('Pandoc did not execute properly!')
+  }
 }
 
 
