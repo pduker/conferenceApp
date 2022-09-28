8f1bd378490a509fd676b79a54d03e22dc2b83de
Patrick Taylor <patricktaylor48@outlook.com>
2022-09-28 11:37:36 -0400

    Server Made

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const server = express()
+
+server.use(bodyParser.json())
+server.get("/", async function(req, res){
+    console.log("Jaydon is weird")
+    res.send("String")
+})
+
+server.listen(8080)
+console.log("Done")
