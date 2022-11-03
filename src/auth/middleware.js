41069aa893832ddd2f1bfb2c325d4dd618337017
Jaydon Reap <jaydonreap@gmail.com>
2022-11-03 10:23:37 -0400

    Improved durability of middleware

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -2,7 +2,15 @@ const { verifyToken } = require('./auth')
 
 async function authMiddleware(req, res, next){
   const raw = req.headers.authorization
+  
+  if (!raw) {
+    res.status(403).send({ status:'error', data: 'Unauthorized' })
+    return
+  }
+
+  //HTTP requires the token be in format "Bearer <token>" we only care about the token so parse it out here
   const token = raw.split(' ')[1]
+
   const result = await verifyToken(token)
 
   if (result){
