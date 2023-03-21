b0be59c638df58cf6ea6ee9ced4d79e6fdacd071
Benjamin Raymon <bendraymon@gmail.com>
2023-03-21 11:20:59 -0400

    added put and delete endpoints for days

--- src/database/days.js
+++ src/database/days.js
@@ -1,4 +1,5 @@
 const { Days, Sessions, Papers, Authors, SuppMaterials } = require('./db')
+const { updateChangedFields } = require('./utils')
 
 async function getAllDays() {
   const days = await Days.findAll({ include: [
@@ -26,7 +27,32 @@ async function createDay(weekday, date) {
   return day
 }
 
+async function updateDay (newDay) {
+  const currDay = await Days.findByPk(newDay.id)
+
+  if (!currDay) {
+    throw new Error('Could not find an existing day with that ID')
+  }
+
+  updateChangedFields(currDay, newDay)
+
+  await currDay.save()
+}
+
+async function deleteDay (dayId) {
+  const day = await Days.findByPk(dayId)
+
+  if (!day) {
+    throw new Error('Could not find a day that matched that ID')
+  }
+
+  await day.destroy()
+}
+
+
 module.exports = {
   getAllDays,
-  createDay
+  createDay,
+  updateDay,
+  deleteDay
 }
