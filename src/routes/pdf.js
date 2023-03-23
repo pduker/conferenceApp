d3e67ea45e786414bac7f4cfe575a910cf3e8b05
Jaydon Reap <jaydonreap@gmail.com>
2023-03-23 15:01:18 -0400

    Added PDF routes

---
+++ src/routes/pdf.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { PDFTeX } = require('texlive')
+
+const router = express.Router()
+
+router.post('/', async function (req, res) {
+  try {
+    const { weekday, date } = req.body
+
+    if (!weekday || !date) {
+      res.status(400).send('Bad request, missing required fields')
+      return
+    }
+
+    const day = await createDay(weekday, date)
+
+    res.json(day)
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
+})
+
+
+module.exports = router
