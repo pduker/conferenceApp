748ca4c321a8631925a142a505ea2dbb73901400
Aaron Gluck <67354344+overinvested@users.noreply.github.com>
2023-04-11 14:11:19 -0400

    begin formatting

--- src/routes/pdf.js
+++ src/routes/pdf.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const PDFDocument = require('pdfkit')
-const fs = require('fs')
-
+const fs = require('fs');
 const router = express.Router()
 
 router.post('/', async function (req, res) {
   try {
+    schedule = req.body
     let doc = new PDFDocument({bufferPages: true})
     doc.pipe(res);
-    doc.font('Times-Roman')
-     .fontSize(12)
-     .text(JSON.stringify(req.body));
+    doc.image('../static/SMT_Logo.png', 430, 15, {align: 'center'})
+    for (day in schedule) {
+      doc.font('Times-Roman')
+      .fontSize(14)
+      .text(JSON.stringify(day));
+      for (session in day) {
+        doc.font('Times-Roman')
+        .fontSize(11)
+        .text(JSON.stringify(session));
+        for (paper in session) {
+          doc.font('Times-Roman')
+          .fontSize(11)
+          .text(JSON.stringify(paper));
+        }
+      }
+    }
+    
 
     res.type('application/pdf');
     res.attachment('test.pdf');
