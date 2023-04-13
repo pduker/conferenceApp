dcb59035863132e89274bdea59da53bb83c6eaec
Aaron Gluck <67354344+overinvested@users.noreply.github.com>
2023-04-13 15:00:03 -0400

    begin abstract implemetation

--- src/routes/pdf.js
+++ src/routes/pdf.js
@@ -9,47 +9,64 @@ router.post('/', async function (req, res) {
     let doc = new PDFDocument({bufferPages: true})
     doc.pipe(res);
     // doc.image('../static/SMT_Logo.png', 430, 15, {align: 'center'})
-    for (day in schedule) {
-      if (parseInt(day) != 0) {
-        doc.text(' ', {paragraphGap:9})
-      }
-      doc.font('Helvetica-Bold')
-      .fontSize(14)
-      .text(schedule[day]['weekday'].toUpperCase(), {paragraphGap:12})
-      // add spacing and line between day and sessions
-      let sessions = schedule[day]['Sessions']
-      for (let session in sessions) {
-        // add spacing and line between sessions
-        if (session) {
-          doc.font('Helvetica-Bold')
-          .fontSize(11)
-          .text(sessions[session]['title'].toUpperCase())
-          doc.font('Helvetica')
-          .fontSize(11)
-          .text(sessions[session]['chair'] + ', Chair', {paragraphGap:9})
-          let papers = schedule[day]['Sessions'][session]['Papers']
-          for (let paper in papers) {
-            // add spacing between papers
-            doc.font('Helvetica-Bold')
+    doc.registerFont('Proxima', 'src/static/proxima_nova_font.otf')
+    doc.registerFont('Proxima-Italic', 'src/static/proxima_italic.otf')
+    doc.registerFont('Proxima-Bold', 'src/static/proxima_bold.otf')
+    for (let i = 0; i<2; i++) {
+          
+      for (day in schedule) {
+        // if (parseInt(day) != 0) {
+        //   doc
+        //   .fontSize(1)
+        //   .text(' '.repeat(129*14), {paragraphGap:9, underline:true})
+        // }
+        doc.font('Proxima-Bold')
+        .fontSize(14)
+        .text(schedule[day]['weekday'].toUpperCase())
+        doc
+        .fontSize(1)
+        .text(' '.repeat(129*14), {underline:true, paragraphGap:14})
+        // add spacing and line between day and sessions
+        let sessions = schedule[day]['Sessions']
+        for (let session in sessions) {
+          // add spacing and line between sessions
+          if (session) {
+            doc.font('Proxima-Bold')
             .fontSize(11)
-            .text(papers[paper]['title'])
-            let authors = ""
-            let author_list = schedule[day]['Sessions'][session]['Papers'][paper]['Authors']
-            for (let author in author_list) {
-              if (parseInt(author) != 0) {
-                authors += ", "
+            .text(sessions[session]['title'].toUpperCase(), {continued:true}).font('Times-Roman').text('this is a test')
+            doc.font('Proxima')
+            .fontSize(11)
+            .text(sessions[session]['chair'] + ', Chair', {paragraphGap:9})
+            let papers = schedule[day]['Sessions'][session]['Papers']
+            for (let paper in papers) {
+              // add spacing between papers
+              doc.font('Proxima-Bold')
+              .fontSize(11)
+              .text(papers[paper]['title'])
+              let authors = ""
+              let author_list = schedule[day]['Sessions'][session]['Papers'][paper]['Authors']
+              for (let author in author_list) {
+                if (parseInt(author) != 0) {
+                  authors += ", "
+                }
+                authors += author_list[author]['name']
+                + ` (${author_list[author]['institution']})`
               }
-              authors += author_list[author]['name']
-              + ` (${author_list[author]['institution']})`
-            }
-            doc.font('Helvetica')
-            .fontSize(11)
-            .text(authors, {paragraphGap:9})
+              doc.font('Proxima')
+              .fontSize(11)
+              .text(authors, {paragraphGap:9})
+              if (i == 1) {
+                doc.font('Proxima')
+                .text(papers[paper]['abstract'],{align: 'justify'})
+              }
+            }            
+            doc
+            .fontSize(1)
+            .text(' '.repeat(129*14), {underline:true, paragraphGap:14})
           }
         }
       }
     }
-    
 
     res.type('application/pdf');
     res.attachment('test.pdf');
