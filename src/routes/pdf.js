eb52c406615d00e0870442e04eb03825b34b8ea4
Aaron Gluck <67354344+overinvested@users.noreply.github.com>
2023-04-25 15:40:49 -0400

    separate sessions by start time

--- src/routes/pdf.js
+++ src/routes/pdf.js
@@ -17,17 +17,26 @@ router.post('/', async function (req, res) {
     doc.registerFont('Proxima-Italic', 'src/static/proxima_italic.otf')
     doc.registerFont('Proxima-Bold', 'src/static/proxima_bold.otf')
     for (let i = 0; i<2; i++) {
-          
+      
       for (let day in schedule) {
-        doc.font('Proxima-Bold')
-        .fontSize(14)
-        .text(schedule[day]['weekday'].toUpperCase())
-        doc
-        .fontSize(1)
-        .text(' '.repeat(129*14), {underline:true, paragraphGap:14})
+
         let sessions = schedule[day]['Sessions']
-        for (let session in sessions) {
-          if (session) {
+        sessions = sessions.sort((a,b) => sortSessionsByTime(a, b))
+        
+        for (let session in sessions) {          
+          if (sessions[session]) {
+            let previous = session > 0 ? sessions[session - 1] : -1;
+
+            let period = getDayPeriod(sessions[session]['start'])
+            if (previous != -1) {
+              if (sessions[session]['start'] !== previous['start'] || sessions[session]['end'] !== previous['end']) {
+                drawDayHeader(doc, schedule[day]['weekday'], period, sessions[session]['start'], sessions[session]['end'])
+              }
+            }
+            else {
+              drawDayHeader(doc, schedule[day]['weekday'], period, sessions[session]['start'], sessions[session]['end'])
+            }
+
             doc.font('Proxima-Bold')
             .fontSize(11)
             .text(sessions[session]['title'].toUpperCase())
@@ -96,10 +105,12 @@ router.post('/', async function (req, res) {
                 
                 
               }
-            }            
-            doc
-            .fontSize(1)
-            .text(' '.repeat(129*14), {underline:true, paragraphGap:14})
+            }        
+            if (previous != -1 && (sessions[session]['start'] !== previous['start'] || sessions[session]['end'] !== previous['end'])){
+              doc
+              .fontSize(1)
+              .text(' '.repeat(129*14), {underline:true, paragraphGap:14})  
+            }    
           }
         }
       }
@@ -115,5 +126,60 @@ router.post('/', async function (req, res) {
   }
 });
 
+function sortSessionsByTime(a, b){
+    if (convertTo24HourString(a.start) === convertTo24HourString(b.start))
+      return convertTo24HourString(a.end) < convertTo24HourString(b.end) ? -1 : 1
+    else
+      return convertTo24HourString(a.start) < convertTo24HourString(b.start) ? -1 : 1
+}
+
+function convertTo24HourString (time) {
+  const splitTime = time.split(" ") // get the 10:00 and PM of "10:00 PM"
+  const hoursAndMinutes = splitTime[0].split(":") // get the 10 and 00 of 10:00
+  const hours = parseInt(hoursAndMinutes[0]) // convert hours to a number (for offsets)
+  const minutes = hoursAndMinutes[1] // get the minutes (still a string since we don't need to offset this)
+
+  if (splitTime[1] === "PM" || splitTime[1] === "pm") {
+      let final
+
+      if (hours === 12) {
+          final = `${hours}:${minutes}`
+      } else {
+          final = `${hours + 12}:${minutes}`
+      }
+      
+      return final
+  } else {
+    if (hours.length < 2)
+      return `0${hours}:${minutes}` // Needs to be in full 24HR format still so we have a leading 0
+    else 
+      return `${hours}:${minutes}`
+  }
+}
+
+
+function getDayPeriod(time) {
+  if (convertTo24HourString(time) < '12:00') {
+    return 'Morning'
+  }
+  else if (convertTo24HourString(time) < '16:00') {
+    return 'Afternoon'
+  }
+  else {
+    return 'Evening'
+  }
+}
+
+function drawDayHeader(doc, weekday, period, start, end) {
+  doc
+  .fontSize(0)
+  .text(' ', {paragraphGap:14})
+  doc.font('Proxima-Bold')
+  .fontSize(14)
+  .text(weekday.toUpperCase() + ' ' + period.toUpperCase() + ' - ' + start + ' - ' + end)
+  doc
+  .fontSize(1)
+  .text(' '.repeat(129*14), {underline:true, paragraphGap:14})
+}
 
 module.exports = router
